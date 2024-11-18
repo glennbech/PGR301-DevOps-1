d7cf12abbca0ca8792dff1076b3625be4f894473
EC2 Default User <ec2-user@ip-172-31-43-211.eu-west-1.compute.internal>
2024-11-18 19:33:19 +0000

    Add Terraform GitHub Actions workflow for automated infrastructure deployment

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -36,4 +36,12 @@ resource "aws_lambda_function" "image_lambda" {
 resource "aws_lambda_event_source_mapping" "sqs_trigger" {
   event_source_arn = aws_sqs_queue.image_generation_queue.arn
   function_name    = aws_lambda_function.image_lambda.arn
+
+  batch_size = 10
+  enabled    = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
+
