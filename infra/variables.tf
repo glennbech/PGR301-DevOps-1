572e3e8380e7cd302d894c81d0435b4c3cdb1a97
EC2 Default User <ec2-user@ip-172-31-43-211.eu-west-1.compute.internal>
2024-11-18 18:36:53 +0000

    Add Terraform GitHub Actions workflow

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,19 +7,19 @@ variable "bucket_name" {
 variable "lambda_role_name" {
   description = "IAM Role for Lambda function"
   type        = string
-  default     = "ha_lambda_role_"
+  default     = "ha_lambda_role"
 }
 
 variable "lambda_policy_name" {
   description = "IAM Policy name for Lambda function"
   type        = string
-  default     = "lambda_policy_" 
+  default     = "lambda_policy" 
 }
 
 variable "lambda_function_name" {
   description = "Lambda function name"
   type        = string
-  default     = "image_generation_lambda_v2"  # Endret for å unngå konflikt
+  default     = "image_generation_lambda"  
 }
 
 variable "sqs_queue_name" {
