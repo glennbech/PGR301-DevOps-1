52b3a3ad24b6d7f7852fdb27d9c276bbf74cf912
EC2 Default User <ec2-user@ip-172-31-43-211.eu-west-1.compute.internal>
2024-11-19 15:52:33 +0000

    small changes

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,13 @@
 variable "prefix" {
   description = "Unique prefix for all resource names"
   type        = string
+  deafult     = "104"
 }
 
 variable "bucket_name" {
   description = "S3 bucket name for storing data"
   type        = string
+  default     = "pgr301-couch-explorers"
 }
 
 variable "sqs_queue_name" {
